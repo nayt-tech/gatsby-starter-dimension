190424c3c242ee2bd4f442c6daa549e041311036
Jay <j.gao@outlook.com>
2022-01-13 13:34:16 +0800

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
+    title: 'Nayt Capital Limited',
     author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    description: 'To be filled',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,16 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      {/* <span className="icon fa-diamond"></span> */}
+      <span class="fa-brands fa-artstation"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Nayt Capital Limited</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          A ...... 
           <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          and .......
         </p>
       </div>
     </div>

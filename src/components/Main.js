ab8c10325fc52fb895d749334b808e88f09dc3fe
Jay <j.gao@outlook.com>
2022-01-13 17:18:14 +0800

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -84,8 +84,8 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          {/* <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <h2 className="major">Contact</h2>
+          {/* <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
@@ -106,35 +106,14 @@ class Main extends React.Component {
                 <input type="reset" value="Reset" />
               </li>
             </ul>
-          </form> */}
-          {/* <ul className="icons">
+          </form>  */}
+           <ul className="icons">
             <li>
-              <a
-                href="https://twitter.com/HuntaroSan"
-                className="icon fa-twitter"
-              >
-                <span className="label">Twitter</span>
+              <a href="enquiries@nayt.io" className="icon fa-email">
+                <span className="label">Email</span>
               </a>
             </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
-                className="icon fa-github"
-              >
-                <span className="label">GitHub</span>
-              </a>
-            </li> */}
-          {/* </ul> */}
+          </ul>
           {close}
         </article>
       </div>
